8247febe4d4bb42e415d39590a4288cc7ab67fa6
Isabella Mia Nielsen <isabellamianielsen@gmail.com>
2024-02-23 20:06:12 +0100

    optimized vector alignment

--- simulation/data_query.py
+++ simulation/data_query.py
@@ -11,15 +11,15 @@ class Data_Query:
         return self.rawData.sensordata
     
 
-    def directionToNormal(self):
+    def directionToNormal(self, force_):
         """
             Calulates the direction the robot should turn to align with the surface normal
             Returns: Euler angles for rotation
         """
         force = self.getFTData()
+        force = force_
         force_norm = force / np.linalg.norm(force) # Normalize the force vector to be unit
-        y_axis = [0, 1, 0] # Axis to align with
-        angle =  np.argcos(np.dot(force_norm, y_axis)) # Rotation angle
-        rotation_axis = np.cross(force_norm, y_axis) # Orthorgonal vector (axis to rotate around)
-        axis_angle = rotation_axis * angle # Axis angle representation
-        return Rotation.from_rotvec(axis_angle).as_euler({'x', 'y', 'z'}, degrees=False) # To euler angles
+        y_axis = np.atleast_2d([0, 1, 0]) # Axis to align with
+        rot = Rotation.align_vectors(y_axis, force_norm)[0] # Align force to y axis
+        return rot.as_euler({'x', 'y', 'z'}, degrees=False) # To euler angles
+    
