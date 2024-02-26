47769d8f02ead2571b937840b3611f3acd74cc0d
Sniklo <nikoneathery@gmail.com>
2024-02-26 19:18:43 +0100

    NIKO added forward and inverse k shtuff, we can alter final config by giving a string to the inverse solver that will prioritize certain things

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
 from simulation.simulator import MJ
 from simulation.data_query import Data_Query 
 from robot.robot_control import Robot_Controler
+from time import sleep
+import roboticstoolbox as rtb
+import numpy as np
 
 
 if __name__ == "__main__":
-  simulator = MJ()
-  
+  robot = rtb.DHRobot(
+      [ 
+          rtb.RevoluteDH(d=0.1625, alpha = np.pi/2),
+          rtb.RevoluteDH(a=-0.425),
+          rtb.RevoluteDH(a=-0.3922),
+          rtb.RevoluteDH(d=0.1333, alpha=np.pi/2),
+          rtb.RevoluteDH(d=0.0997, alpha=-np.pi/2),
+          rtb.RevoluteDH(d=0.0996)
+      ], name="UR5e"
+                  )
+
+  simulator = MJ(robot)
+    # Universal Robot UR5e kiematics parameters 
+
+
   simulator.start()
   robot_data = Data_Query(simulator.d)
-  controller = Robot_Controler(simulator=simulator, sim_data=robot_data)
-  jointValues = [0,0,0,0,0,0]
-
+  controller = Robot_Controler(simulator=simulator, sim_data=robot_data,robot=simulator.robot)
+  fk = controller.forKin([-0.3, 0, -2.2, 0, 2, 0.7854])
+  ik = controller.invKin(fk)
 
   while 1:
-    print(robot_data.getFTData())
-    controller.sendJoint(jointValues)
+  
+    controller.sendJoint(ik.q)
+    print(ik)
+    
   
     
--- robot/robot_control.py
+++ robot/robot_control.py
@@ -1,16 +1,27 @@
 from simulation.simulator import MJ
 from simulation.data_query import Data_Query
+import roboticstoolbox as rtb
 
 
 
 class Robot_Controler:
-    def __init__(self, simulator: MJ, sim_data: Data_Query):
+    def __init__(self, simulator: MJ, sim_data: Data_Query, robot:rtb.DHRobot):
         self.simulator = simulator
-        pass
+        self.robot = robot
+
+        
 
 
-    def sendJoint(self,join_values):
+    def sendJoint(self, join_values):
         with self.simulator.jointLock:
           for i in range(0, 6):
             self.simulator.joints[i] = join_values[i]
           self.sendPositions = True
+
+    def forKin(self,q):
+        return self.robot.fkine(q)
+
+
+    def invKin(self, desiredTCP):
+          q = self.robot.ikine_LM(desiredTCP)
+          return q
